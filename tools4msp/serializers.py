baabf27f45b9bd8d3563a79c3c36689003f78e81
Stefano Menegon <ste.menegon@gmail.com>
2019-07-16 22:01:27 +0200

    removed deprecated serializer

--- tools4msp/serializers.py
+++ tools4msp/serializers.py
@@ -1,24 +1,39 @@
 from rest_framework import serializers
 from rest_framework_gis.serializers import GeoModelSerializer
-from .models import CaseStudy, CaseStudyLayer, CaseStudyInput
+from .models import CaseStudy, CaseStudyLayer, CaseStudyInput, \
+    CodedLabel
+
+
+class CodedLabelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CodedLabel
+        fields = ('code',)
 
 
 class CaseStudyLayerSerializer(serializers.ModelSerializer):
+    coded_label = serializers.SlugField(source="coded_label.code",
+                                        read_only=False)
+
     class Meta:
         model = CaseStudyLayer
-        fields = ('layer_type', 'layerfile', 'casestudy')
+        fields = ('coded_label',
+                  'file')
 
 
 class CaseStudyInputSerializer(serializers.ModelSerializer):
+    coded_label = serializers.SlugField(source="coded_label.code",
+                                        read_only=False)
     class Meta:
         model = CaseStudyInput
-        fields = ('input_type',
-                  'inputfile',)
+        fields = ('coded_label',
+                  'file',)
 
 
-class CaseStudySerializer(GeoModelSerializer):
+
+class CaseStudySerializer(serializers.ModelSerializer):
     layers = CaseStudyLayerSerializer(many=True, read_only=True)
     inputs = CaseStudyInputSerializer(many=True, read_only=True)
+    # extent = serializers.JSONField(read_only=True)
 
     class Meta:
         model = CaseStudy
