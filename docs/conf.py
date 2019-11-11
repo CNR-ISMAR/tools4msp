9fbe26f6c5483b97c68a29f20fd3263f0fbd913e
Stefano Menegon <ste.menegon@gmail.com>
2019-11-11 12:28:06 +0100

    updated release number for documentation

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, CNR-ISMAR'
 author = 'CNR-ISMAR'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0'
+release = '2.0'
 
 
 # -- General configuration ---------------------------------------------------
