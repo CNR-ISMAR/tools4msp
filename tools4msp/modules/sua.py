7ef6c55cdeec416c606b88a55c5a6136a2881d83
Stefano Menegon <ste.menegon@gmail.com>
2021-04-08 09:02:10 +0200

    added parallel computing for SUA

--- tools4msp/modules/sua.py
+++ tools4msp/modules/sua.py
@@ -3,18 +3,22 @@ import numpy as np
 import pandas as pd
 from SALib.sample import saltelli
 from SALib.analyze import sobol
+from joblib import Parallel, delayed
+from copy import deepcopy
 
 logger = logging.getLogger('tools4msp.sua')
 
 
-def run_sua(module_cs, nparams, nruns, kwargs_run={}):
+def run_sua(module_cs, nparams, nruns, bygroup=True, n_jobs=1, kwargs_run={}):
     module_cs.load_layers()
     module_cs.load_grid()
     module_cs.load_inputs()
     module_cs.run(**kwargs_run)
 
-    module_cs_sua = CEACaseStudySUA(module_cs, nparams=nparams, kwargs_run=kwargs_run)
-    module_cs_sua.runall(nruns)
+    module_cs_sua = CEACaseStudySUA(module_cs, nparams=nparams,
+                                    bygroup=bygroup,
+                                    kwargs_run=kwargs_run)
+    module_cs_sua.runall(nruns, n_jobs=n_jobs)
 
     return module_cs_sua
 
@@ -24,7 +28,7 @@ class CaseStudySUA(object):
     This is a base class for support Sensitivity and Uncertainty Analysis.
     Child classes have to implement "set_problem" and "set_params" methods.
     """
-    def __init__(self, module_cs, nparams=40, kwargs_run={}):
+    def __init__(self, module_cs, nparams=40, bygroup=True, kwargs_run={}):
         self.module_cs = module_cs
         self.kwargs_run = kwargs_run
         # check runtypelevel
@@ -38,13 +42,17 @@ class CaseStudySUA(object):
             'names': [],  # ['mscf'],  # , 'rfunc_b', 'rfuncb_m'],
             'bounds': [],  # [[0, 1]],  # , [1, 30], [0.3, 0.7]],
             'dists': [],  # ['unif'],  # 'unif', 'unif'],
-            'groups': [],  # ['mscf'],  # 'rfunc', 'rfunc']
+            # 'groups': [],  # ['mscf'],  # 'rfunc', 'rfunc']
         }
+
+        if bygroup:
+            self.problem['groups'] = []
 
         self.var_index = []
         self.model_output_stats = None
         self.target_values = None
         self.nparams = nparams
+        self.bygroup = bygroup
 
     def set_problem(self):
         """This is override by child classes"""
@@ -60,10 +68,12 @@ class CaseStudySUA(object):
         self.problem['names'].append(name)
         self.problem['bounds'].append(bound)
         self.problem['dists'].append(dist)
-        if group is not None:
+        if group is not None and self.bygroup:
             self.problem['groups'].append(group)
-        else:
+        elif group is None and self.bygroup:
             self.problem['groups'].append(name)
+        else:
+            pass
 
         self.var_index.append(var_idx)
 
@@ -71,19 +81,32 @@ class CaseStudySUA(object):
         return saltelli.sample(self.problem, nruns,
                                calc_second_order=calc_second_order)
 
-    def runall(self, nruns, calc_second_order=False):
+    def runall(self, nruns, calc_second_order=False, n_jobs=1):
         self.set_problem()
         samples = self.sample(nruns, calc_second_order)
         model_output_stats = RunningStats2D(percentiles=[25, 50, 75])
-        target_values = []
-        for i, params in enumerate(samples):
-            self.set_params(params)
-            self.module_cs.run(runtypelevel=0, **self.kwargs_run)
-            model_output = self.module_cs.get_main_output()
+        # TODO: add parallelization
+        def _single_run(i, params):
+            module_cs = deepcopy(self.module_cs)
+            self.set_params(params, module_cs=module_cs)
+            module_cs.run(runtypelevel=0, **self.kwargs_run)
+            model_output = module_cs.get_main_output()
             model_output_stats.push(model_output)
-            target_value = self.module_cs.get_SUA_target()
-            target_values.append(target_value)
+            target_value = module_cs.get_SUA_target()
             logger.debug('run {} target={}'.format(i, target_value))
+            return target_value
+            
+        with Parallel(n_jobs=n_jobs, backend='threading') as parallel:
+            target_values = parallel(delayed(_single_run)(i, params) for i, params in enumerate(samples))
+
+        # for i, params in enumerate(samples):
+        #     self.set_params(params)
+        #     self.module_cs.run(runtypelevel=0, **self.kwargs_run)
+        #     model_output = self.module_cs.get_main_output()
+        #     model_output_stats.push(model_output)
+        #     target_value = self.module_cs.get_SUA_target()
+        #     target_values.append(target_value)
+        #     logger.debug('run {} target={}'.format(i, target_value))
         self.model_output_stats = model_output_stats
         self.target_values = np.array(target_values)
 
@@ -200,7 +223,7 @@ class CEACaseStudySUA(CaseStudySUA):
                                  [sensitivity_score,
                                   int_confidence],
                                  'triang',
-                                 'sensitivity'
+                                 'sensitivity' if self.bygroup else None
                                  )
 
         weighs = module_cs.weights
@@ -227,19 +250,21 @@ class CEACaseStudySUA(CaseStudySUA):
                                  [weight,
                                   int_confidence],
                                  'triang',
-                                 'weight'
+                                 'weight' if self.bygroup else None
                                  )
 
             self.add_problem_var(['weights', 'distance', label],
                                  [distance,
                                   int_confidence],
                                  'triang',
-                                 'distance'
+                                 'distance' if self.bygroup else None
                                  )
 
-    def set_params(self, params):
+    def set_params(self, params, module_cs=None):
+        if module_cs is None:
+            module_cs = self.module_cs
         for i, (var_type, var_column, var_name) in enumerate(self.var_index):
-            df = getattr(self.module_cs, var_type)
+            df = getattr(module_cs, var_type)
             # print(obj[obj.sua_var_name == var_name])
             val = params[i]
             if var_column == 'distance':
